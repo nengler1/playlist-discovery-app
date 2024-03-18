8c4be446c2337bbdaa2e4163864a427dd42d50c6
nengler <nengler@uccs.edu>
2024-03-18 13:14:28 -0600

    Added playlist app

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'playlist_app'
 ]
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
